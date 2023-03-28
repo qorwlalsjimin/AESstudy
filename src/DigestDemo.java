402faaaf8abe77b48da76c7de3e5628a5bbc4cf4
Jimin Baek <qorwlalsjimin@naver.com>
2023-03-28 15:33:00 +0900

    #6 주석 추가

--- src/DigestDemo.java
+++ src/DigestDemo.java
@@ -6,15 +6,20 @@ import java.security.NoSuchProviderException;
 import java.security.Security;
 
 //참고 https://stackoverflow.com/questions/64679961/how-to-use-bouncycastle-with-java
+//AES는 아님
 public class DigestDemo
 {
     public static void main(String[] args) throws Exception
     {
         Security.addProvider(new BouncyCastleProvider());
-        System.out.println(byteArrayToHex(computeDigest("SHA-256", "Hello World!".getBytes())));
+        System.out.println(
+                byteArrayToHex(
+                        computeDigest("SHA-256", "Hello World!".getBytes())
+                )
+        );
     }
 
-    public static String byteArrayToHex(byte[] a)
+    public static String byteArrayToHex(byte[] a) //16진수로 바꿈
     {
         StringBuilder sb = new StringBuilder(a.length * 2);
         for(byte b: a)
