a4e6ded4798521e3ca64372765fe85e3d9a0441c
지민 백 <s2104@e-mirim.hs.kr>
2023-03-30 20:05:39 +0900

    #6 두번째 예제: 코드 이해&주석 , iv 전역변수로 바꾸기

--- src/CBC.java
+++ src/CBC.java
@@ -7,12 +7,12 @@ import java.security.Key;
 
 //참고 http://www.fun25.co.kr/blog/java-aes128-cbc-encrypt-decrypt-example
 public class CBC {
+    String iv = "0987654321654321"; //IV 초기화
     public Key getAESKey() throws Exception {
-        String iv;
         Key keySpec;
 
-        String key = "1234567890123456";
-        iv = key.substring(0, 16);
+        String key = "1234567890123456"; //키값
+//        iv = key.substring(0, 16); //왜 필요한지 모르겠음
         byte[] keyBytes = new byte[16];
         byte[] b = key.getBytes("UTF-8");
 
@@ -21,31 +21,29 @@ public class CBC {
             len = keyBytes.length;
         }
 
-        System.arraycopy(b, 0, keyBytes, 0, len);
-        keySpec = new SecretKeySpec(keyBytes, "AES");
+        System.arraycopy(b, 0, keyBytes, 0, len); //키값 Bytes로 변환해서 배열에 저장
+        keySpec = new SecretKeySpec(keyBytes, "AES"); //키값으로 AES키값 만들기
 
-        return keySpec;
+        return keySpec; //AES 키값
     }
 
     // 암호화
     public String encAES(String str) throws Exception {
-        Key keySpec = getAESKey();
-        String iv = "0987654321654321";
-        Cipher c = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        c.init(Cipher.ENCRYPT_MODE, keySpec, new IvParameterSpec(iv.getBytes("UTF-8")));
-        byte[] encrypted = c.doFinal(str.getBytes("UTF-8"));
-        String enStr = new String(Base64.encode(encrypted));
+        Key keySpec = getAESKey(); //AES키값
+        Cipher c = Cipher.getInstance("AES/CBC/PKCS5Padding"); //암호화 수행해주는 Chiper 객체 생성
+        c.init(Cipher.ENCRYPT_MODE, keySpec, new IvParameterSpec(iv.getBytes("UTF-8"))); //암호화 모드 설정, AES키, IV로 초기화
+        byte[] encrypted = c.doFinal(str.getBytes("UTF-8")); //AES 암호화
+        String enStr = new String(Base64.encode(encrypted)); //암호문 Base64 인코딩해서 문자열로 변환
         return enStr;
     }
 
     // 복호화
     public String decAES(String enStr) throws Exception {
-        Key keySpec = getAESKey();
-        String iv = "0987654321654321";
-        Cipher c = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        c.init(Cipher.DECRYPT_MODE, keySpec, new IvParameterSpec(iv.getBytes("UTF-8")));
-        byte[] byteStr = Base64.decode(enStr.getBytes("UTF-8"));
-        String decStr = new String(c.doFinal(byteStr), "UTF-8");
+        Key keySpec = getAESKey(); //AES키값
+        Cipher c = Cipher.getInstance("AES/CBC/PKCS5Padding"); //복호화 수행해주는 Cipher 객체 생성
+        c.init(Cipher.DECRYPT_MODE, keySpec, new IvParameterSpec(iv.getBytes("UTF-8"))); //복호화 모드, AES키, IV로 초기화
+        byte[] byteStr = Base64.decode(enStr.getBytes("UTF-8")); //암호문 Base64 디코딩
+        String decStr = new String(c.doFinal(byteStr), "UTF-8"); //복호화해서 문자열로 변환
 
         return decStr;
     }
