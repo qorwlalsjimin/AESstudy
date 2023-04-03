648f16021b4e3728293cee11bc56774812031cb3
Jimin Baek <qorwlalsjimin@naver.com>
2023-04-03 10:21:35 +0900

    #6 설명 주석 추가

--- src/AES256byBaek.java
+++ src/AES256byBaek.java
@@ -4,42 +4,54 @@ import javax.crypto.*;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.UnsupportedEncodingException;
-import java.security.*;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 
 public class AES256byBaek {
     byte[] iv;
-    Key keySpec;
+    SecretKeySpec keySpec;
 
     /** 생성자 */
     public AES256byBaek() throws UnsupportedEncodingException, NoSuchAlgorithmException {
         //1. 256비트 비밀키 생성
         KeyGenerator kgen = KeyGenerator.getInstance("AES");
-        kgen.init(256); //256비트(32바이트)
-        byte[] keyBytes = kgen.generateKey().getEncoded();
+        kgen.init(256); //256비트(32바이트) 키 지정
+        byte[] keyBytes = kgen.generateKey().getEncoded(); //byte[]로 키 저장
 
         //2. iv 자동 생성
         iv = new byte[16];
         new SecureRandom().nextBytes(iv);
 
         //3. keySpec 생성
-        SecretKeySpec keySpec = new SecretKeySpec(keyBytes, "AES");
-        this.keySpec = keySpec;
+        keySpec = new SecretKeySpec(keyBytes, "AES");
     }
 
     /** 암호화 */
-    public String encryption(String str) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, UnsupportedEncodingException {
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        cipher.init(Cipher.ENCRYPT_MODE, keySpec, new IvParameterSpec(iv));
-        byte[] en_arr = cipher.doFinal(str.getBytes("UTF-8"));
-        return new String(Base64.encode(en_arr));
+    public String encryption(String str) throws NoSuchPaddingException, NoSuchAlgorithmException, //Cipher.getInstance()
+            InvalidAlgorithmParameterException, InvalidKeyException, //cipher.init()
+            UnsupportedEncodingException, //str.getBytes()
+            IllegalBlockSizeException, BadPaddingException /*doFinal()*/ {
+
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding"); //암호화를 수행해주는 Cipher 객체
+        cipher.init(Cipher.ENCRYPT_MODE, keySpec, new IvParameterSpec(iv)); //(암호화 모드, 키스펙, iv)로 초기화
+        byte[] en_arr = cipher.doFinal(str.getBytes("UTF-8")); //암호화 수행
+
+        return new String(Base64.encode(en_arr)); //AES 암호문(byte[])을 => Base64 인코딩(byte[])해주고 => 문자열로 변환
     }
 
     /** 복호화 */
-    public String decryption(String en_str) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, UnsupportedEncodingException {
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        cipher.init(Cipher.DECRYPT_MODE, keySpec, new IvParameterSpec(iv));
-        byte[] b = Base64.decode(en_str.getBytes());
-        return new String(cipher.doFinal(b), "UTF-8");
+    public String decryption(String en_str) throws NoSuchPaddingException, NoSuchAlgorithmException,
+            InvalidAlgorithmParameterException, InvalidKeyException,
+            UnsupportedEncodingException,
+            IllegalBlockSizeException, BadPaddingException {
+
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding"); //복호화를 수행해주는 Cipher 객체
+        cipher.init(Cipher.DECRYPT_MODE, keySpec, new IvParameterSpec(iv)); //(복호화 모드, 키스펙, iv)로 초기화
+        byte[] b = Base64.decode(en_str.getBytes()); //Base64 디코딩
+
+        return new String(cipher.doFinal(b), "UTF-8"); //복호화 수행 => 문자열로 변환
     }
 }
 
@@ -47,9 +59,11 @@ public class AES256byBaek {
 class AES256byBaekExam{
     public static void main(String[] args) throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, UnsupportedEncodingException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
         AES256byBaek aes = new AES256byBaek();
-        String en = aes.encryption("010-1111-2222");
-        String de = aes.decryption(en);
-        System.out.println(en);
-        System.out.println(de);
+
+        String en = aes.encryption("010-1111-2222"); //암호화할 데이터 입력
+        String de = aes.decryption(en); //복호화할 암호문 입력
+
+        System.out.println("암호문: "+en);
+        System.out.println("원문: "+de);
     }
 }
